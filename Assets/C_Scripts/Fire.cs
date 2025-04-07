07bdca12435c1892f2b58d7460fbd1e50169c2b4
TommasoPistoi <tommasopistoi9@gmail.com>
2025-04-07 17:30:26 +0200

    Start Script for the Attack

---
+++ Assets/C_Scripts/Fire.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+
+public class Example : MonoBehaviour
+{
+    private float speed = 2f;
+    public GameObject Palla;
+
+    private void OnTriggerEnter(Collider Palla)
+    {
+        if{ GetComponent  }
+
+    }
+}
