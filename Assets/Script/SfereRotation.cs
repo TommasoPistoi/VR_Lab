c75370de0eabd1686c5ad0acd7f0d3a83b557542
NoemiSossella <noemi.sossella@gmail.com>
2025-04-08 10:49:36 +0200

    Balle che roteano

--- Assets/Script/SfereRotation.cs
+++ Assets/Script/SfereRotation.cs
@@ -5,10 +5,17 @@ public class SfereRotation : MonoBehaviour
     public float degreesPerSecond = 90f; // Velocità di rotazione in gradi al secondo
     public Vector3 rotationAxis = Vector3.up; // Asse di rotazione (default: asse Y)
     private bool isRotating = false; // Variabile per controllare la rotazione
+    private inputtesting inputtesting;
+
+
+    void Start()
+    {
+        inputtesting = FindAnyObjectByType<inputtesting>();
+    }
 
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.P)) // Controlla se il tasto 'P' è premuto
+        if (inputtesting.Indice_Right_bool) // Controlla se il tasto 'P' è premuto
         {
             isRotating = !isRotating; // Inverte lo stato di rotazione
         }
